aed9ddb1ed582fb0c7b61540632856b16993e053
Mark Andrew Murtagh <mamurtag@tcd.ie>
2016-05-25 23:19:25 +0100

    Initial Commit

---
+++ WakeUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WakeUp
+//
+//  Created by Mark Murtagh on 25/05/2016.
+//  Copyright © 2016 MAVM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
